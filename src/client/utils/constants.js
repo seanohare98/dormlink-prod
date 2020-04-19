547f552814b80fb82ba90dec1fdeb4870cb02d7f
poilk199881 <61458883+poilk199881@users.noreply.github.com>
2020-04-19 16:56:11 +0800

    Update constants.js
    Add the list of hostels of colleges

--- src/client/utils/constants.js
+++ src/client/utils/constants.js
@@ -1,6 +1,45 @@
 exports.hostelInfo = [
+  //CC
   { name: 'PMHC High Block', affiliation: 'Chung Chi' },
   { name: 'PMHC Low Block', affiliation: 'Chung Chi' },
   { name: 'Madam SH Ho Hall', affiliation: 'Chung Chi' },
-  { name: 'Daisy Li Hall', affiliation: 'New Asia' }
+  { name: 'Hua Lien Tang', affiliation: 'Chung Chi' },
+  { name: 'Lee Shu Pui Hall', affiliation: 'Chung Chi' },
+  { name: 'Ming Hua Tang', affiliation: 'Chung Chi' },
+  { name: 'Theology Building', affiliation: 'Chung Chi' },
+  { name: 'Wen Chih Tang', affiliation: 'Chung Chi' },
+  { name: 'Wen Lin Tang', affiliation: 'Chung Chi' },
+  { name: 'Ying Lin Tang', affiliation: 'Chung Chi' },
+  
+  //NA
+  { name: 'Daisy Li Hall', affiliation: 'New Asia' },
+  { name: 'Chih Hsing Hall', affiliation: 'New Asia' },
+  { name: 'Xuesi Hall', affiliation: 'New Asia' },
+  { name: 'Grace Tien Hall', affiliation: 'New Asia' },
+  
+  //UC
+  { name: 'Adam Schall Residence', affiliation: 'United College' },
+  { name: 'Bethlehem Hall', affiliation: 'United College' },
+  { name: 'Chan Chun Ha Hostel', affiliation: 'United College' },
+  { name: 'Hang Seng Hall', affiliation: 'United College' },
+  
+  //SC
+  { name: 'Kuo Mou Hall', affiliation: 'Shaw College' },
+  { name: 'Student Hostel II (Yat Sen Hall)', affiliation: 'Shaw College' },
+  
+  //MC
+  { name: 'Morningside Hostel', affiliation: 'Morningside College' },
+  
+  //SH
+  { name: 'S.H. Ho Student Hostel', affiliation: 'S.H. Ho College' },
+  
+  //CW
+  { name: 'C.W.Chu Student Hostel', affiliation: 'C.W.Chu Colledge' },
+  
+  //YS
+  { name: 'Wu Yee Sun Student Hostel', affiliation: 'Wu Yee Sun College' },
+  
+  //WS
+  { name: 'Lee Woo Sing Student Hostel', affiliation: 'Lee Woo Sing College' },
+  
 ];
