a38efea0c359e02df224a35791080a013fed3755
Charlie Li <charlieli@hotmail.com.tw>
2020-04-22 22:12:11 +0800

    UserCard

--- src/client/components/SimilarUserCards.js
+++ src/client/components/SimilarUserCards.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -16,35 +17,35 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import { Divider } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import { red } from '@material-ui/core/colors';
 import avatar from '../../../public/avatar.jpg';
 
 const useStyles = makeStyles(theme => ({
-  grid: {
-    width: '100%',
-    display: 'grid',
-    gridAutoRows: '200px',
-    gridTemplateColumns: '1fr',
-    columnGap: '10px',
-    rowGap: '15px'
-  },
   card: {
+    maxWidth: 320,
     width: '100%',
-    height: '100%'
+    height: '100%',
+    margin: 'auto'
   },
   media: {
     paddingTop: '56.25%' // 16:9
   },
   content: {
     flexGrow: 1
+  },
+  avatar: {
+    backgroundColor: red[500]
   }
 }));
 
 const levels = {
-  0: 'No Way',
-  25: 'Not really',
-  50: 'sometimes',
-  75: 'pretty much',
-  100: 'definitely'
+  0: '⭐',
+  25: '⭐⭐',
+  50: '⭐⭐⭐',
+  75: '⭐⭐⭐⭐',
+  100: '⭐⭐⭐⭐⭐'
 };
 
 const UserCard = props => {
@@ -58,49 +59,49 @@ const UserCard = props => {
   if (!data) return <p>Not found</p>;
 
   return (
-    <Container className={classes.grid} maxWidth='md'>
-      {/* End hero unit */}
-      <Grid container spacing={4}>
-        <Grid item key={data.userSid.sid} xs={12} sm={6} md={4}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image='https://i.picsum.photos/id/743/200/200.jpg'
-              title={data.userSid.firstName + data.userSid.lastName}
-            />
-            <CardContent className={classes.content}>
-              <Typography gutterBottom variant='h5' component='h2'>
-                {`${data.userSid.firstName} ${data.userSid.lastName}`}
-              </Typography>
-              <Typography>
-                {`Sleeps late: ${levels[data.userSid.schedule]}`}
-              </Typography>
-              <Typography>
-                {`Keeps Clean: ${levels[data.userSid.cleanliness]}`}
-              </Typography>
-              <Typography>
-                {`Active in the community: ${
-                  levels[data.userSid.participation]
-                }`}
-              </Typography>
-              <Typography>
-                {`A+ Student: ${levels[data.userSid.studious]}`}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size='small' color='primary'>
-                View
-                {data.userSid.firstName}
-                's Profile
-              </Button>
-              <Button size='small' color='primary'>
-                Match with {data.userSid.firstName}
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </Container>
+    <div>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {data.userSid.firstName[0] + data.userSid.lastName[0]}
+            </Avatar>
+          }
+          title={data.userSid.firstName + ' ' + data.userSid.lastName}
+          titleTypographyProps={{ variant: 'h5' }}
+        />
+        <Divider />
+        <CardContent className={classes.content}>
+          <table style={{ width: '80%', margin: 'auto' }}>
+            <tr>
+              <td style={{ width: '50%' }}>Night Owl: </td>
+              <td>{levels[data.userSid.schedule]}</td>
+            </tr>
+            <tr>
+              <td style={{ width: '50%' }}>Cleanliness: </td>
+              <td>{levels[data.userSid.cleanliness]}</td>
+            </tr>
+            <tr>
+              <td style={{ width: '50%' }}>Scocial: </td>
+              <td>{levels[data.userSid.participation]}</td>
+            </tr>
+            <tr>
+              <td style={{ width: '50%' }}>Studious: </td>
+              <td>{levels[data.userSid.studious]}</td>
+            </tr>
+          </table>
+        </CardContent>
+        <CardActions>
+          <Button size='small' color='primary'>
+            {'View ' + data.userSid.firstName + "'s Profile"}
+          </Button>
+          <Button size='small' color='primary'>
+            {'Match with ' + data.userSid.firstName}
+          </Button>
+        </CardActions>
+      </Card>
+      <div style={{ height: '20px', width: '100%', clear: 'both' }}></div>
+    </div>
   );
 };
 
@@ -114,8 +115,9 @@ export default () => {
   if (error) return <p>Error :(</p>;
   if (!data) return <p>Not found</p>;
 
-  if (data)
-    return data.similar.map(({ sid, age, gender }) => (
-      <UserCard key={sid} sid={sid} gender={gender} age={age} />
-    ));
+  const userCards = data.similar.map(({ sid, age, gender }) => (
+    <UserCard key={sid} sid={sid} gender={gender} age={age} />
+  ));
+
+  return userCards;
 };
